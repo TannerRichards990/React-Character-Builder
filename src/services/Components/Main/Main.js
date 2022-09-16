e16e4ee1c9960b12c7f1be8bcdd917560fcb212a
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-16 12:01:53 -0700

    built slogan js so that slogans will appear on main and can be imported

---
+++ src/services/Components/Main/Main.js
@@ -0,0 +1,5 @@
+import './Main.css';
+
+import { useState } from 'react';
+import Controls from '../Controls/Controls';
+import Car from '../Car/Car';
---
+++ src/services/Components/Slogan/Slogan.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './slogan.css';
+
+export default function Slogan({ sloganArr }) {
+  const slogans = sloganArr.map((slogan, index) => {
+    <p key={index}>{Slogan}</p>;
+  });
+
+  return (
+    <div className="slogan">
+      {slogans}
+    </div>
+  );
+}
+
